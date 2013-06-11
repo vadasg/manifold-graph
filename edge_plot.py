4fce13d9453f95a3e3adcfd37fcf9f9abce8e7c8
Vadas Gintautas <vadasg@gmail.com>
2013-06-11 10:37:19 -0400

    updated graphs per TO DOs

--- edge_plot.py
+++ edge_plot.py
@@ -34,36 +34,24 @@ fig.subplots_adjust(top=0.95,bottom=0.1,left=0.10,right=0.93,hspace=0.35,wspace=
 
 ax = fig.add_subplot(1,1,1)
 
-diameters = [ 1, 1.63, 2, 2.43, 3, 3.26, 4, 4.87 ]
-steps =[ '1', 'H', '2', 'J', '3', '2H', '4', '2J' ]
-counts = [ 3, 184, 409, 2445, 1060, 582, 93, 11 ] 
-
-diameters1= [ 1, 2, 3, 4, ]
-counts1= [ 3, 409, 1060, 93 ] 
-
-diameters2 = [ 1.63, 2.43, 3.26, 4.87 ]
-steps2 =[ 'H', 'J', '2H', '2J' ]
-counts2 = [ 184, 2445, 582, 11 ] 
+diameters = [ 1, 2, 3, 4, 5 ]
+counts = [ 3, 593,3505, 675, 11 ] 
 
 #ax.plot(diameters,counts,'k--',alpha=0.5)
-for d,c in zip(diameters1,counts1):
+for d,c in zip(diameters,counts):
     ax.plot([d,d],[1,c],'k--')
-
-for d,c,s in zip(diameters2,counts2,steps2):
-    ax.plot([d,d],[2.3,c],'k--')
-    ax.text(d,1.3,s,horizontalalignment='center')
 
 ax.semilogy(diameters,counts,'ro')
 #rects1 = ax.bar(ind+width, procutMeans, width, color='r', yerr=procutStd)
 #rects1 = ax.bar(ind, procutMeans, width, color='r', yerr=procutStd)
 # add some
-ax.set_xlabel('Diameter')
+ax.set_xlabel('Diameter with only Edges')
 #ax.xaxis.set_label_coords(0.5, -0.12)
 ax.set_ylabel('Count (log scale)')
 ax.set_title('Global statistics')
-ax.set_xticks([0] + diameters + [5,6])
+ax.set_xticks([0] + diameters + [6])
 #ax.set_yticks(yt)
-ax.set_xticklabels( ['0','1','','2','','3','','4','','5','6'], horizontalalignment='center')
+ax.set_xticklabels( ['0','1','2','3','4','5','6'], horizontalalignment='center')
 a=ax.axis([0,6,1,numpy.max(counts)*5])
 
 
@@ -72,5 +60,5 @@ a=ax.axis([0,6,1,numpy.max(counts)*5])
 
 
 
-pylab.savefig('./paper/figures/global_statistics.png', dpi=300)
+pylab.savefig('./paper/figures/edge_diameters.png', dpi=300)
 pylab.show()
--- split_plot.py
+++ split_plot.py
@@ -27,7 +27,7 @@ fig_size=(csize,csize*1.618)
 
 fig = pylab.figure()
 
-fig.subplots_adjust(top=0.95,bottom=0.1,left=0.10,right=0.93,hspace=0.35,wspace=0.2)
+fig.subplots_adjust(top=0.95,bottom=0.10,left=0.10,right=0.93,hspace=0.35,wspace=0.2)
 
 # this is plot 1 with Procut orange data
 
@@ -37,40 +37,49 @@ ax = fig.add_subplot(1,1,1)
 diameters = [ 1, 1.63, 2, 2.43, 3, 3.26, 4, 4.87 ]
 steps =[ '1', 'H', '2', 'J', '3', '2H', '4', '2J' ]
 counts = [ 3, 184, 409, 2445, 1060, 582, 93, 11 ] 
+types = [ '$S^{3}/Q$',  '$L(4,1)$' ,'$L(3,1)$' ,'$RP^{3}$' ,'$S^{3}$' ]
+markers = ['rs','ws','bx','ks','gs']
 
-diameters1= [ 1, 2, 3, 4, ]
-counts1= [ 3, 409, 1060, 93 ] 
+countd = {}
+countd[ '$S^{3}/Q$'] = [ 0, 1, 0, 0, 0, 0, 0, 0 ]
+countd[ '$L(4,1)$' ] = [ 0, 0, 0, 1, 0, 0, 0, 0 ]
+countd[ '$L(3,1)$' ] = [ 0, 0, 1, 1, 0, 0, 0, 0 ]
+countd[ '$RP^{3}$' ] = [ 0,10, 7, 5, 0, 0, 0, 0 ]
+countd[ '$S^{3}$'  ]  = [ 3, 173, 401, 2438, 1060, 582, 93, 11 ]
 
-diameters2 = [ 1.63, 2.43, 3.26, 4.87 ]
-steps2 =[ 'H', 'J', '2H', '2J' ]
-counts2 = [ 184, 2445, 582, 11 ] 
+#$1$ &  &  &  &   &3    & 3    \\
+#$2$ & 1&  &1 &17 &574  & 593  \\
+#$3$ &  &1 &1 &5  &3498 & 3505 \\
+#$4$ &  &  &  &   &675  & 675   \\
+#$5$ &  &  &  &   &11   & 11   \\
+#Total&1 &1 &2 &22   &4761   &4787  \\
+
+
+
 
 #ax.plot(diameters,counts,'k--',alpha=0.5)
-for d,c in zip(diameters1,counts1):
-    ax.plot([d,d],[1,c],'k--')
+for t,m in zip(types,markers):
+    ax.semilogy(diameters,countd[t],m,ms=10,label=t,mew=1)
+    for d,c,s in zip(diameters,counts,steps):
+        ax.plot([d,d],[0.7,c],'k--')
+    #    ax.text(d,c*1.5, s,horizontalalignment='center')
 
-for d,c,s in zip(diameters2,counts2,steps2):
-    ax.plot([d,d],[2.3,c],'k--')
-    ax.text(d,1.3,s,horizontalalignment='center')
-
-ax.semilogy(diameters,counts,'ro')
 #rects1 = ax.bar(ind+width, procutMeans, width, color='r', yerr=procutStd)
 #rects1 = ax.bar(ind, procutMeans, width, color='r', yerr=procutStd)
 # add some
 ax.set_xlabel('Diameter')
-#ax.xaxis.set_label_coords(0.5, -0.12)
 ax.set_ylabel('Count (log scale)')
 ax.set_title('Global statistics')
-ax.set_xticks([0] + diameters + [5,6])
+ax.set_xticks(diameters)
 #ax.set_yticks(yt)
-ax.set_xticklabels( ['0','1','','2','','3','','4','','5','6'], horizontalalignment='center')
-a=ax.axis([0,6,1,numpy.max(counts)*5])
-
+ax.set_xticklabels( steps , horizontalalignment='center')
+ax.axis([0,numpy.max(diameters)*1.1,0.7,numpy.max(counts)*5])
+ax.legend(numpoints=1)
 
 
 #ax.legend( ('Procut Orange',), loc=1 ) #add trailing comma to make it a tuple so full word is printed
 
 
 
-pylab.savefig('./paper/figures/global_statistics.png', dpi=300)
+pylab.savefig('./paper/figures/split_statistics.png', dpi=300)
 pylab.show()
